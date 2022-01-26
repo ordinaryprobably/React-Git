3fdc2a2d9fdbd84a03f2d0d0c1ee771ba69d64f3
ahiou <ahiou1ahiou@gmail.com>
2022-01-26 17:52:14 +0900

    change: shinchon

--- react-lifecycle/src/ZenQuote.js
+++ react-lifecycle/src/ZenQuote.js
@@ -3,21 +3,26 @@ import axios from 'axios';
 import './ZenQuote.css';
 
 export default class ZenQuote extends Component {
-  state = { quote: null };
+  state = { quote: '' };
 
-  async componentDidMount() {
-    const quote = await axios.get("https://api.github.com/zen");
-    
-    setTimeout(() => {
-      this.setState({ quote: quote.data })
-    }, 3000);
+  componentDidMount() {
+    axios.get("https://api.github.com/zen").then(response => {
+      setTimeout(() => {
+        this.setState({ quote: response.data });
+      }, 3000);
+    })
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log(prevState);
+    console.log(this.state)
   }
 
   render() {
     return(
       <div>
-        <div className={!this.state.quote ? "loader" : null}></div>
         <h1>Zen Quotes..</h1>
+        <div className={!this.state.quote ? "loader" : null}></div>
         <p>{this.state.quote}</p>
       </div>
     )
