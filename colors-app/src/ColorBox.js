1eb6485dcdd006c765224a3cc4081263445cd093
ahiou <ahiou1ahiou@gmail.com>
2022-02-02 15:00:48 +0900

    FEAT: added ColorBox.js

---
+++ colors-app/src/ColorBox.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import './ColorBox.css';
+
+export default class ColorBox extends Component {
+  render() {
+    return (
+      <div className="ColorBox" style={{ backgroundColor: this.props.background }}>
+        <span>{this.props.name}</span>
+        <span>More</span>
+      </div>
+    )
+  }
+}
--- colors-app/src/Palette.js
+++ colors-app/src/Palette.js
@@ -1,7 +1,21 @@
 import React, { Component } from "react";
+import ColorBox from "./ColorBox";
+import './Palette.css';
 
 export default class Palette extends Component {
   render() {
-    return <h1>Palete</h1>;
+    const colorBoxes = this.props.colors.map(tag => (
+      <ColorBox background={tag.color} name={tag.name} />
+    ))
+
+    return (
+      <div className="Palette">
+        {/* NavBar goes here */}
+        <div className="Palette-colors">
+          { colorBoxes }
+        </div>
+        {/* Footer goes here */}
+      </div>
+    )
   }
 }
