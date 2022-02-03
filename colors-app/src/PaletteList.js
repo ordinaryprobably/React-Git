0566738718b334b04dce8bcf184f67b13bc87d33
ahiou <ahiou1ahiou@gmail.com>
2022-02-03 16:07:01 +0900

    FEAT: installed material-ui/styles

--- colors-app/src/PaletteList.js
+++ colors-app/src/PaletteList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import MiniPalette from './MiniPalette';
 
 export default class PaletteList extends Component {
   render() {
@@ -7,6 +8,7 @@ export default class PaletteList extends Component {
 
     return (
       <div>
+        <MiniPalette />
         {palette.map(palette => (
           <p>
             <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
