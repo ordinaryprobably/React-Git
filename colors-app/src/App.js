236094eb25ec734e265c04b8d18a550ddc389753
ahiou <ahiou1ahiou@gmail.com>
2022-02-03 15:26:19 +0900

    FEAT: added routes for palettes

--- colors-app/src/App.js
+++ colors-app/src/App.js
@@ -5,17 +5,18 @@ import generatePalette from './ColorHelpers';
 import { Route, Switch } from "react-router";
 
 export default class App extends Component {
-  render() {
-    const palette = generatePalette(seedColors[1]);
+  findPalette(id) {
+    return seedColors.find(palette => palette.id === id);
+  }
 
+  render() {
     return (
       <Switch>
         <Route exact path='/' render={() => <h1>PaletteList goes here</h1>} />
-        <Route exact path='/palette/:id' render={() => <h1>individual Palette goes here</h1>} />
+        <Route exact path='/palette/:id' 
+          render={(routerProps) => <Palette palette={generatePalette(this.findPalette(routerProps.match.params.id))}/>} 
+        />
       </Switch>
-      // <div>
-      //   <Palette palette={palette}/>
-      // </div>
     )
   }
 }
