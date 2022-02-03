a62064c2da0abf46d40eaea1d633b32946930b1c
ahiou <ahiou1ahiou@gmail.com>
2022-02-03 22:25:12 +0900

    FEAT: used chroma.luminance to adjust text color based on background

--- colors-app/src/ColorBox.js
+++ colors-app/src/ColorBox.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { Link } from "react-router-dom";
+import chroma from "chroma-js";
 import './ColorBox.css';
 
 export default class ColorBox extends Component {
@@ -25,24 +26,26 @@ export default class ColorBox extends Component {
   render() {
     const { background, name, urlToSingleColorPalette, showLink } = this.props;
     const { copied } = this.state;
+    const isTooDark = chroma(background).luminance() <= 0.08;
+    const isTooLight = chroma(background).luminance() >= 0.8;
 
     return (
       <CopyToClipboard text={background} onCopy={this.changeCopyState}>
         <div className="ColorBox" style={{ backgroundColor: background }}>
           <div className={`copy-overlay ${copied && 'show'}`} style={{ backgroundColor: background }}/>
           <div className={`copy-message ${copied && 'show'}`}>
-            <h1>copied!</h1>
-            <p>{background}</p>
+            <h1 className={isTooLight && 'dark-text'}>copied!</h1>
+            <p className={isTooLight && 'dark-text'}>{background}</p>
           </div>
           <div className="copy-container">
             <div className="box-content">
-              <span>{name}</span>
+              <span className={isTooDark && 'light-text'}>{name}</span>
             </div>
             <button className="copy-button">copy</button>
           </div>
           {showLink && 
             <Link to={urlToSingleColorPalette} onClick={e => e.stopPropagation()}>
-              <span className="see-more">More</span>
+              <span className={`see-more ${isTooLight && 'dark-text'}`}>More</span>
             </Link>
           }
         </div>
