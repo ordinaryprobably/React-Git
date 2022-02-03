63415c73828fc721155168afd5ff80468aef83b5
ahiou <ahiou1ahiou@gmail.com>
2022-02-03 15:16:34 +0900

    FEAT: added react router and installed peer for @mui/material

--- colors-app/src/App.js
+++ colors-app/src/App.js
@@ -2,14 +2,20 @@ import React, { Component } from "react";
 import Palette from "./Palette";
 import seedColors from "./SeedColors";
 import generatePalette from './ColorHelpers';
+import { Route, Switch } from "react-router";
 
 export default class App extends Component {
   render() {
     const palette = generatePalette(seedColors[1]);
+
     return (
-      <div>
-        <Palette palette={palette}/>
-      </div>
+      <Switch>
+        <Route exact path='/' render={() => <h1>PaletteList goes here</h1>} />
+        <Route exact path='/palette/:id' render={() => <h1>individual Palette goes here</h1>} />
+      </Switch>
+      // <div>
+      //   <Palette palette={palette}/>
+      // </div>
     )
   }
 }
