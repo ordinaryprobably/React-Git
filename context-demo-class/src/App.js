8f9b42f9155a573f57b9fc8961822e77537be61b
ahiou <ahiou1ahiou@gmail.com>
2022-02-06 12:10:59 +0900

    MINOR_FIX: directory change

---
+++ context-demo-class/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import Navbar from "./Navbar";
+import Form from "./Form";
+import PageContent from "./PageContent";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import { LanguageProvider } from "./contexts/LanguageContext";
+
+/**
+ * Provider 는 value 를 사용하고 싶은 컴포넌트만 감싸면 된다.
+ * <ThemeProvider> 와 <LanguageProvider> 의 위치가 다른 이유다.
+ */
+
+class App extends Component {
+  render() {
+    return (
+      <ThemeProvider>
+        <PageContent>
+          <LanguageProvider>  
+            <Navbar />
+            <Form />
+          </LanguageProvider>
+        </PageContent>
+      </ThemeProvider>
+    );
+  }
+}
+
+export default App;
